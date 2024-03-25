2a76cfa1b90537286d4d421b8869586d9bbe12d9
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-03-26 00:26:15 +1100

    Added player control and issue test #1

---
+++ moving_entities/movement_base.gd
@@ -0,0 +1,43 @@
+extends Node
+class_name MovementBase
+#Authored by Ethan. Please consult for any modifications or major feature requests.
+
+#region Variables
+	#Signals
+
+	#Enums
+
+	#Constants
+
+	#Exported Variables
+	#@export_group("Group")
+	#@export_subgroup("Subgroup")
+@export var movement_speed : float = 100
+
+	#Onready Variables
+var direction: Vector2
+
+var can_input: bool = true
+
+	#Other Variables (please try to separate and organise!)
+
+#endregion
+
+#region Godot methods
+func _physics_process(_delta):
+	if(can_input):
+		owner.velocity = direction * movement_speed
+	else:
+		owner.velocity = Vector2(0,0)
+	
+	owner.move_and_slide()
+#endregion
+
+#region Signal methods
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+func set_direction(dir: Vector2):
+	direction = dir
+#endregion
