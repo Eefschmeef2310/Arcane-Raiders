70c553578313f8e0fff6a0e25a1dbd929deac0da
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-03-29 11:39:52 +1100

    added a mutliplayer spawner

---
+++ online/testSpawner.gd
@@ -0,0 +1,54 @@
+extends MultiplayerSpawner
+
+@export var playerScene : PackedScene
+var players = {}
+#class_name
+#Authored by Tom. Please consult for any modifications or major feature requests.
+
+#region Variables
+#Signals
+
+#Enums
+
+#Constants
+
+#Exported Variables
+#@export_group("Group")
+#@export_subgroup("Subgroup")
+
+#Onready Variables
+
+#Other Variables (please try to separate and organise!)
+
+#endregion
+
+#region Godot methods
+func _ready():
+	#Runs when all children have entered the tree
+	spawn_function = spawnPlayer 
+	if is_multiplayer_authority():
+		spawn(1)
+		multiplayer.peer_connected.connect(spawn)
+		multiplayer.peer_disconnected.connect(removePlayer)
+
+func _process(delta):
+	#Runs per frame
+	if Input.is_action_just_pressed("spawn"):
+		spawn(1)
+#endregion
+
+#region Signal methods
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+func spawnPlayer(data):
+	var p = playerScene.instantiate()
+	p.set_multiplayer_authority(data)
+	players[data] = p
+	return p
+
+func removePlayer(data):
+	players[data].queue_free()
+	players.erase(data)
+#endregion
