15b28560d92f994adec913ffd8cce28774799f54
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-04-05 12:39:35 +1100

    simplified rpc calls, hopefully stable now?

--- multiplayer/multiplayerLobby/lobbyCardManager.gd
+++ multiplayer/multiplayerLobby/lobbyCardManager.gd
@@ -21,6 +21,10 @@ extends Node
 @export_group("Other Resources")
 @export var default_slot_icon : Texture2D
 
+@export_group("References")
+@export var lobby_manager : Node
+
+
 #Onready Variables
 
 #Other Variables (please try to separate and organise!)
@@ -39,7 +43,8 @@ func _process(delta):
 	if(Input.is_action_just_pressed("debug_random")):
 		if(Steam.getPersonaName() == player_name.text):
 			#raider_desc.text += " •⩊• "
-			rpc("setValues", default_slot_icon, "Cool Player", str(raider_desc.text + " •⩊• "), Steam.getPersonaName())
+			##rpc("setValues", default_slot_icon, "Cool Player", str(raider_desc.text + " •⩊• "), Steam.getPersonaName())
+			lobby_manager.rpc("UpdateCard", SteamManager.player_id, default_slot_icon, "Cool Player", str(raider_desc.text + " •⩊• "), Steam.getPersonaName())
 	pass
 #endregion
 
@@ -48,17 +53,19 @@ func _process(delta):
 #endregion
 
 #region Other methods (please try to separate and organise!)
-@rpc("any_peer","call_local")
-func setValues(portrait : Texture2D, raider : String, description : String, username : String):
-	raider_portrait.texture = portrait
-	raider_name.text = raider
-	raider_desc.text = description
-	player_name.text = username
-	#also set colour ect
+#@rpc("any_peer","call_local")
+#func setValues(portrait : Texture2D, raider : String, description : String, username : String):
+	#raider_portrait.texture = portrait
+	#raider_name.text = raider
+	#raider_desc.text = description
+	#player_name.text = username
+	##also set colour ect
 
 func setLocalDefault():
-	setValues(default_slot_icon, "???", "Press any key to join", " ")
+	#setValues(default_slot_icon, "???", "Press any key to join", " ")
+	pass
 
 func setOnlineDefault():
-	setValues(default_slot_icon, "???", "Join through the lobby", " ")
+	#setValues(default_slot_icon, "???", "Join through the lobby", " ")
+	pass
 #endregion
