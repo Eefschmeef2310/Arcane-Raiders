36805362f9b6bed5dcc1c294e4e7c5c15851be10
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-04-06 19:35:53 +1100

    raider and laodouts moved to their own resource

---
+++ multiplayer/multiplayerLobby/loadoutRes.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name LoadoutRes
+
+@export var loadout_name : String
+@export var loadout_desc : String
+# loadout desc is probably temporary, we can change this to an array of starter
+# spells and display them as icons in the spot instead!
