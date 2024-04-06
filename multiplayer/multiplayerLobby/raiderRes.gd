7293977bb56efc9f0f203785826e2e90dfbec52f
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-04-06 20:32:21 +1100

    got rid of the texture2d in raiderRes

--- multiplayer/multiplayerLobby/raiderRes.gd
+++ multiplayer/multiplayerLobby/raiderRes.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name RaiderRes
 
-@export var portrait : Texture2D #gotta convert this to a path when sending!
+@export var portraitString : String  #gotta convert this to a path when sending!
 @export var raider_name : String
 @export var raider_desc : String
 @export var color : Color
