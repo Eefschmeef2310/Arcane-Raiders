b161e2b51f1ce8583619bb07966d27618bb072bd
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-04-06 19:03:00 +1100

    bunch of changes to player cards and started making resource

---
+++ multiplayer/multiplayerLobby/playerCardResource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name PlayerCardRes
+
+@export_group("Data")
+@export var portrait_path : String
+@export var display_username : bool
+@export var username : String 
+
+@export_group("Usage")
