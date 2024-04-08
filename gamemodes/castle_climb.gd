99a9ad8d3a2a5a6ca9be923a3e1d886127113ee4
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-04-08 21:09:16 +1000

    castle climb seed

--- gamemodes/castle_climb.gd
+++ gamemodes/castle_climb.gd
@@ -7,8 +7,8 @@ extends Node
 
 @export var basic_rooms: Array[PackedScene]
 
-var number_of_players = 4
-var seed : int = 6969
+var number_of_players = 1
+var rng_floors: RandomNumberGenerator = RandomNumberGenerator.new()
 var current_floor : int = -1
 
 var current_room_node : Node2D
@@ -16,6 +16,7 @@ var current_room_node : Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if start_on_spawn:
+		set_number_of_players(number_of_players)
 		start_climb()
 
 func _process(delta):
@@ -23,11 +24,7 @@ func _process(delta):
 		start_next_floor()
 
 func start_climb():
-	var i = 0
-	for child in $GameUI/PlayerUIContainer.get_children():
-		if i >= number_of_players:
-			child.hide()
-		i += 1
+	# Do any server-sided stuff here
 	start_next_floor()
 
 func start_next_floor():
@@ -48,7 +45,7 @@ func start_next_floor():
 	# Spawn new room
 	print("Creating new room.")
 	level_spawner.spawn_function = spawn_basic_level
-	level_spawner.spawn(randi_range(0, basic_rooms.size() - 1))
+	level_spawner.spawn(rng_floors.randi_range(0, basic_rooms.size() - 1))
 	
 	await get_tree().create_timer(0.2).timeout
 	current_room_node.spawn_players(number_of_players)
@@ -60,7 +57,6 @@ func spawn_basic_level(index: int) -> Node:
 
 @rpc("authority", "call_local", "reliable")
 func play_room_transition(last_floor: int, next_floor: int):
-	$RunUI/FloorLabel.text = get_floor_name(current_floor)
 	$RoomTransitionUI/Items/VBoxContainer/NextFloorLabel.text = get_floor_name(current_floor)
 	$RoomTransitionUI/Items/VBoxContainer/LastFloorLabel.text = get_floor_name(current_floor-1)
 	$RoomTransitionUI/AnimationPlayer.play("next_floor")
@@ -68,6 +64,19 @@ func play_room_transition(last_floor: int, next_floor: int):
 	tween.tween_property($RoomTransitionUI/Items, "modulate:a", 1, 0.25)
 	tween.tween_interval(2)
 	tween.tween_property($RoomTransitionUI/Items, "modulate:a", 0, 0.25)
+	
+	await get_tree().create_timer(0.5).timeout
+	$RunUI/FloorLabel.text = get_floor_name(current_floor)
+
+func set_number_of_players(n: int):
+	number_of_players = n
+	var i = 0
+	for child in $GameUI/PlayerUIContainer.get_children():
+		if i >= number_of_players:
+			child.hide()
+		else:
+			child.show()
+		i += 1
 
 func get_floor_name(floor: int) -> String:
 	if floor < 0:
