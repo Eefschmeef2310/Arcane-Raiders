20178d51293df36bd277d4d0b6a24802b9786e4f
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-04-14 16:13:45 +1000

    asd

--- debug/test_room.gd
+++ debug/test_room.gd
@@ -9,6 +9,9 @@ func _ready():
 	for player in get_tree().get_nodes_in_group("player"):
 		dynamic_camera.add_target(player)
 
+func _ready():
+	$DynamicCamera.add_target($Player)
+
 func _process(_delta):
 	if Input.is_action_just_pressed("debug_random") and data_to_fuck_with:
 		for i in data_to_fuck_with.spells.size():
