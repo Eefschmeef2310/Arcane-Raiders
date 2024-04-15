51caadbd8017bebe91c39ea044546eabea91932b
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-04-15 11:49:04 +1000

    damage number stacking

--- ui/damage_number.gd
+++ ui/damage_number.gd
@@ -15,13 +15,16 @@ func set_s(s: float):
 func set_color(c: Color):
 	label.add_theme_color_override("font_color", c)
 
-func set_number(n: int):
-	_number = n
-	label.text = str(n)
+func add(n: int):
+	_number += n
+	label.text = str(_number)
+	animate()
 
 func animate():
 	var dir = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
 	var dist = randf_range(0, 24)
+	if tween:
+		tween.stop()
 	tween = create_tween()
 	tween.tween_property(self, "global_position", global_position + (dir*dist), 0.1).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
 	tween.parallel().tween_property(self, "scale", Vector2(_s*2, _s*2), 0.1)
