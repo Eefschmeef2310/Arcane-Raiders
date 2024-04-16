5f2efa96b494237ec6f4670d45ed1f85fd63a18a
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-04-16 17:10:23 +1000

    Small bat complete

--- moving_entities/enemies/States/bat_attack.gd
+++ moving_entities/enemies/States/bat_attack.gd
@@ -43,7 +43,6 @@ func enter():
 func update(delta):
 	super.update(delta)
 	enemy.aim_direction = enemy.global_position.direction_to(player.global_position)
-	print("attack")
 	enemy.attempt_cast(0)
 	
 func physics_update(delta):
@@ -58,10 +57,13 @@ func set_position():
 	var distance = player.global_position.distance_to(enemy.global_position)
 	var direction = enemy.global_position.direction_to(player.global_position)
 	var final_direction: Vector2 = Vector2.ZERO
-	if(distance < prefered_range): final_direction = -direction
-	else: final_direction = direction
-	#Add "left or right" diection
 	
+	final_direction = direction if (distance < prefered_range) else -direction
+	
+	#Strafe direction
+	final_direction += direction.rotated(deg_to_rad(90*strafe_dir))
+	if(randi() % 100 < 20*nav_timer_interval):
+		strafe_dir *= -1
 	
 	navigation_agent.target_position = enemy.global_position + 2 * final_direction * nav_timer_interval * enemy.movement_speed
 #endregion
