715c8d321016f793d993768c30db57108b98d938
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-04-16 20:12:26 +1000

    testing real app id

--- multiplayer/steamManager/steamManager.gd
+++ multiplayer/steamManager/steamManager.gd
@@ -4,8 +4,8 @@ var player_id = 0 ## 0 = host/offline, 1-2-3 are players that join
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	OS.set_environment("SteamAppID", str(480))
-	OS.set_environment("SteamGameID", str(480))
+	OS.set_environment("SteamAppID", str(2899410))
+	OS.set_environment("SteamGameID", str(2899410))
 	Steam.steamInitEx()
 	
 	## Spacewar (test app) : 480
