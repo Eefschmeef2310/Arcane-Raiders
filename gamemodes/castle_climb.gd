849d6d4f88ce58fa879be49f056f9886566a3fae
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-04-21 23:35:40 +1000

    test234

--- gamemodes/castle_climb.gd
+++ gamemodes/castle_climb.gd
@@ -111,9 +111,15 @@ func _on_room_exited():
 		start_next_floor()
 
 func _on_room_all_players_dead():
+	if is_multiplayer_authority():
+		game_over.rpc()
+
+@rpc("authority", "call_local", "reliable")
+func game_over():
 	var game_over_screen = load("res://screens/game_over_screen.tscn").instantiate()
 	game_over_screen.game = get_parent()
 	get_tree().root.call_deferred("add_child", game_over_screen)
+	
 
 func _on_spell_change_requested(d: PlayerData, i: int, sp: SpellPickup):
 	print("This code is running.")
