77f8203412d0fc9d21f6e0cf5380a879f1e88782
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-05-12 13:46:46 +1000

    fresh water shader

--- debug/clip_test.gd
+++ debug/clip_test.gd
@@ -1,11 +1,12 @@
 @tool
 extends Sprite2D
 
+var pos : Vector2 = Vector2(-1000,-1000)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	global_position = Vector2.ZERO
+	global_position = pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = Vector2.ZERO
+	global_position = pos
