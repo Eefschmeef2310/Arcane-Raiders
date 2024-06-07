0948ddc060d3032171896d0c4a907e00e8f967ab
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-06-08 01:02:44 +1000

    guide arrow rotates correctly, but doesnt move right

--- rooms/castle_room.gd
+++ rooms/castle_room.gd
@@ -69,7 +69,7 @@ func _ready():
 			number_of_enemies_left = 0
 			total_difficulty_left = wave_total_difficulty[0] * difficulty_modifier * number_of_players_difficulty_scale
 			print("Difficulty modifier: " + str(difficulty_modifier))
-			
+		
 			var arr = EnemyManager.Data.keys()
 			if !spawn_keys.is_empty():
 				arr = spawn_keys
--- ui/guide_arrow.gd
+++ ui/guide_arrow.gd
@@ -15,28 +15,37 @@ extends TextureRect
 @export var exit : Node2D
 @export var camera : Camera2D
 
+@export var screen_margin : Vector2 = Vector2(128, 128) # Margin from the edge of the screen
+
 	# Onready Variables
 
 	# Other Variables (please try to separate and organise!)
-var screen_margin : float = 10.0 # Margin from the edge of the screen
+
 #endregion
 
 #region Godot methods
 func _process(_delta):
-	var screen_size = get_viewport_rect().size
-	var angle = exit.global_position.angle_to_point(camera.global_position) + PI
+	var target_pos = exit.global_position
+	var camera_pos = camera.get_screen_center_position()
 	
-	var x = screen_size.x / 2 * cos(angle) + screen_size.x / 2
-	var y = screen_size.y / 2 * sin(angle) + screen_size.y / 2
+	var direction : Vector2 = (target_pos - camera_pos).normalized()
+	var angle = direction.angle()
+	
+	rotation = angle
+	#position = Vector2(get_viewport_rect().size.x / 2 - 64, get_viewport_rect().size.y / 2 - 64) + direction * 100
+	
+	
+	#var exit_screen_pos : Transform2D = exit.get_screen_transform()
+	#
+	##If exit is offscreen
+	#if exit_screen_pos.origin.x <= 0 || exit_screen_pos.origin.x >= get_viewport_rect().size.x || exit_screen_pos.origin.y <= 0 || exit_screen_pos.origin.y >= get_viewport_rect().size.y:
+		#clamp(exit_screen_pos.origin.x, 0, get_viewport_rect().size.x)
+		#clamp(exit_screen_pos.origin.y, 0, get_viewport_rect().size.y)
+		#
+		## Calculate the position of the TextureRect at the edge of the screen
+		#var edge_pos = camera_pos + direction.normalized() * (get_viewport_rect().size / 2)
+		#position = edge_pos
 
-	# Apply margin to avoid going out of screen bounds
-	x = clamp(x, screen_margin, screen_size.x - screen_margin)
-	y = clamp(y, screen_margin, screen_size.y - screen_margin)
-
-	position = Vector2(x, y)
-
-func _draw():
-	draw_line(exit.global_position, camera.global_position, Color.RED)
 #endregion
 
 #region Signal methods
