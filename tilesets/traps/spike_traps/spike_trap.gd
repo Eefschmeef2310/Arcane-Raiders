77f01ca4d1debc75e3a8aea6497356316439ca45
Cookie <x.akkari@outlook.com>
2024-06-11 21:13:56 +1000

    big fixes

--- tilesets/traps/spike_traps/spike_trap.gd
+++ tilesets/traps/spike_traps/spike_trap.gd
@@ -25,11 +25,12 @@ var should_fire : bool = true
 
 #region Godot methods
 func _ready():
-	var castle_climb : CastleClimb = owner.get_parent().get_parent()
-	if castle_climb and castle_climb.get_current_sector() < first_available_sector:
-		queue_free()
-		
-	get_parent().get_parent().all_waves_cleared.connect(toggle_fire)
+	pass
+	#var castle_climb : CastleClimb = owner.get_parent().get_parent()
+	#if castle_climb and castle_climb.get_current_sector() < first_available_sector:
+		#queue_free()
+		#
+	#get_parent().get_parent().all_waves_cleared.connect(toggle_fire)
 #endregion
 
 #region Signal methods
