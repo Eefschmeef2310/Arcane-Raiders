9bce5a5b0c56302fbcfc991a5dc4bccd32df7eba
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-06-22 22:42:13 +1000

    updated blink spell

--- moving_entities/enemies/bosses/boss_bat/b_bat_blink/b_bat_blink.gd
+++ moving_entities/enemies/bosses/boss_bat/b_bat_blink/b_bat_blink.gd
@@ -3,8 +3,7 @@ class_name BatBlinkSpell
 
 const RAY_CASTS = preload("res://moving_entities/enemies/bosses/boss_bat/b_bat_blink/ray_casts.tscn")
 
-@export var hide_time: float = 0.5
-@export var visible_time: float = 0.5
+@export var particle_duration: float = 0.5
 
 var pos_array: Array #Order is Left, Right, Up, Down
 var center_position: Vector2
@@ -24,26 +23,25 @@ func _ready():
 		
 	center_position = Vector2((pos_array[0].x + pos_array[1].x)/2,(pos_array[2].y + pos_array[3].y)/2)
 
+func caster_visibility(visible: bool):
+	#TODO Play particle effect
+	caster.visible = visible
+	caster.invincible = !visible
+	await get_tree().create_timer(particle_duration).timeout
+
+
 func teleport_to_center():
 	if !caster.is_multiplayer_authority(): return
-	#TODO Play particle effect
-	caster.visible = false
-	await get_tree().create_timer(hide_time).timeout
+	await caster_visibility(false)
 	caster.global_position = center_position
-	#TODO Play particle effect
-	caster.visible = true
-	await get_tree().create_timer(visible_time).timeout
+	await caster_visibility(true)
 
 func teleport_to_random_side():
 	if !caster.is_multiplayer_authority(): return
-	#TODO Play particle effect
-	caster.visible = false
-	await get_tree().create_timer(hide_time).timeout
+	await caster_visibility(false)
 	caster.global_position = center_position
 	var raycast:RayCast2D = $RayCasts/RayCastLeft
 	raycast.target_position = Vector2.LEFT.rotated(deg_to_rad(randi() % 360)) * 9999
 	raycast.force_raycast_update()
 	caster.global_position = raycast.get_collision_point() + raycast.get_collision_normal()*100
-	#TODO Play particle effect
-	caster.visible = true
-	await get_tree().create_timer(visible_time).timeout
+	await caster_visibility(true)
