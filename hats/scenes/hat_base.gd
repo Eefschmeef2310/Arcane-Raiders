2d336e1c8c4c857259170028bd46140c605eae2c
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-07-29 00:31:35 +1000

    health hat implemented

---
+++ hats/scenes/hat_base.gd
@@ -0,0 +1,33 @@
+extends Node
+class_name Hat
+#Authored by Ethan. Please consult for any modifications or major feature requests.
+
+#region Variables
+	#Signals
+
+	#Enums
+
+	#Constants
+
+	#Exported Variables
+	#@export_group("Group")
+	#@export_subgroup("Subgroup")
+
+	#Onready Variables
+@onready var player : Player = get_parent()
+
+	#Other Variables (please try to separate and organise!)
+
+#endregion
+
+#region Godot methods
+
+#endregion
+
+#region Signal methods
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+
+#endregion
---
+++ hats/scenes/health_hat.gd
@@ -0,0 +1,36 @@
+extends Hat
+	#class_name
+#Authored by Ethan. Please consult for any modifications or major feature requests.
+
+#region Variables
+	#Signals
+
+	#Enums
+
+	#Constants
+
+	#Exported Variables
+	#@export_group("Group")
+	#@export_subgroup("Subgroup")
+@export var health_increase : int = 250
+
+	#Onready Variables
+
+	#Other Variables (please try to separate and organise!)
+
+#endregion
+
+#region Godot methods
+func _ready():
+	#player.max_health += health_increase
+	player.set_health(player.health + health_increase)
+	player.data.max_health += health_increase
+#endregion
+
+#region Signal methods
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+
+#endregion
