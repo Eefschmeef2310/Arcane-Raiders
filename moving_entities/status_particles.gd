655a8a9324ca825e2175b672eeff02f98d5ae188
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-10-03 17:23:50 +1000

    affliction partciles

--- moving_entities/status_particles.gd
+++ moving_entities/status_particles.gd
@@ -9,15 +9,23 @@ var parent : Entity
 
 func _ready():
 	# Delete for now.
-	queue_free()
+	# queue_free()
 	
 	if !get_parent() is Entity:
 		queue_free()
 		return
 	
 	parent = get_parent() as Entity
-	$BurnParticles.process_material.emission_box_extents.x = width
+	for child in get_children():
+		child.process_material.emission_box_extents.x = width
+		child.lifetime = lifetime
+		child.emitting = false
 
 func _process(_delta):
 	var keys = parent.current_inflictions_dictionary.keys()
 	$BurnParticles.emitting = SpellManager.elements["burn"] in keys
+	$FrostParticles.emitting = SpellManager.elements["frost"] in keys
+	$ShockParticles.emitting = SpellManager.elements["shock"] in keys
+	$WindParticles.emitting = SpellManager.elements["wind"] in keys
+	$WetParticles.emitting = SpellManager.elements["wet"] in keys
+	$StunParticles.emitting = SpellManager.elements["stun"] in keys
